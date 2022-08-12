7ca497491a5c1987a36a33e5d9000424ca92b7a9
elamartine <estevam.nascimento@escolar.ifrn.edu.br>
2022-08-12 13:50:21 -0300

    cleancss

--- js/cep.js
+++ js/cep.js
@@ -1,31 +1,27 @@
-function buscaCep(){
-  let cep = document.getElementById('cep').value;
-  if(cep !== ""){
+function buscaCep() {
+  let cep = document.getElementById("cep").value;
+  if (cep !== "") {
     let url = "https://brasilapi.com.br/api/cep/v2/{cep}" + cep;
     let req = new XMLHttpRequest();
-    req.open('GET', url);
+    req.open("GET", url);
     req.send();
 
-    req.onload = function(){
-      if(req.status === 200){
+    req.onload = function () {
+      if (req.status === 200) {
         let endereco = JSON.parse(req.response);
-        
+
         document.getElementById("logradouro").value = endereco.street;
         document.getElementById("bairro").value = endereco.neighborhood;
         document.getElementById("cidade").value = endereco.city;
-      }
-      else if(req.status === 404){
-      alert("CEP inválido");
-      }
-      else{
+      } else if (req.status === 404) {
+        alert("CEP inválido");
+      } else {
         alert("Erro ao fazer a requisição");
       }
-    }
-
+    };
   }
-
 }
-window.onload = function(){
+window.onload = function () {
   let cep = document.getElementById("cep");
   cep.addEventListener("blur", buscaCep);
-}
+};
